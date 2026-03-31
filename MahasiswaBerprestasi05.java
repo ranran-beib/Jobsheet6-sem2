efd53ab992def7bbb2ff9a035027e0f298923597
ranran-beib <254107060060@student.polinema.ac.id>
2026-03-31 10:23:48 +0700

    MODIFIKASI INSERTION SORT

--- MahasiswaBerprestasi05.java
+++ MahasiswaBerprestasi05.java
@@ -51,7 +51,7 @@ public class MahasiswaBerprestasi05 {
         Mahasiswa05 temp = listMhs[i];
         int j = i;
 
-        while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
+        while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
             listMhs[j] = listMhs[j - 1];
             j--;
         }
